9a0e43e65a28c7fcbbf58d7a8bec906a509c8d7d
scours <32219601+scours@users.noreply.github.com>
2022-10-05 11:22:05 +0200

    Adding .manifest files

---
+++ manifests/mySubFolder/plastic-packaging.js
@@ -0,0 +1,18 @@
+/*
+ * File: plastic-packaging.js
+ * Contract: EU contract 2022-FR01-KA220-HED-000023509
+ * Project: FitNESS 2 ERASMUS+
+ * File Created: Monday, 3rd October 2022
+ * Author: Steward OUADI
+ * -----
+ * Last Modified: Monday, 3rd October 2022
+ * Modified By: Steward OUADI
+ */
+const plasticPackChildren = [
+  "smart-packaging-from-principles-to-commercially-available-intelligent-packaging-systems",
+  "mySubFolder/plastic-packaging",
+];
+
+console.log("inside plastic.js begin");
+console.log(plasticPackChildren);
+console.log("inside plastic.js end");
---
+++ manifests/smart-packaging-from-principles-to-commercially-available-intelligent-packaging-systems.js
@@ -0,0 +1,15 @@
+/*
+ * File: smart-packaging-from-principles-to-commercially-available-intelligent-packaging-systems.js
+ * Contract: EU contract 2022-FR01-KA220-HED-000023509
+ * Project: FitNESS 2 ERASMUS+
+ * File Created: Monday, 3rd October 2022
+ * Author: Steward OUADI
+ * -----
+ * Last Modified: Monday, 3rd October 2022
+ * Modified By: Steward OUADI
+ */
+const smartChildren = ["child-1", "child-2"];
+
+console.log("inside smart.js begin");
+console.log(smartChildren);
+console.log("inside smart.js end");
