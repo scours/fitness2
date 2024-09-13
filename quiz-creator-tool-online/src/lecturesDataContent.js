6d48824404f093b650d02148cc72982afcfdb75a
scours <32219601+scours@users.noreply.github.com>
2024-09-13 16:52:45 +0200

    Now from the quiz result page, we can also see à link to click on the quiz corresponding to the lecture.

---
+++ quiz-creator-tool-online/src/lecturesDataContent.js
@@ -0,0 +1,153 @@
+/*
+ * File: lecturesDataContent.js
+ * Contract: EU contract 2022-FR01-KA220-HED-000023509
+ * Project: FitNESS 2 ERASMUS+
+ * File Created: Friday, 13th September 2024
+ * Authors: Steward OUADI (AgroParisTech),  Olivier VITRAC (INRAE)
+ * -----
+ * Last Modified: Friday, 13th September 2024
+ * Modified By: Steward OUADI
+ */
+
+const lecturesData = [
+  {
+    id: "glassFoodContactMaterialRecycling",
+    name: "Glass food contact material recycling",
+    url: "https://fitness.agroparistech.fr/fitness2/lectures/lectures.html#Environmental%20pollution/Glass%20food%20contact%20material%20recycling/glass-food-contact-material-recycling",
+  },
+  {
+    id: "metalFoodContactMaterialRecycling",
+    name: "Metal food contact material recycling",
+    url: "https://fitness.agroparistech.fr/fitness2/lectures/lectures.html#Environmental%20pollution/Metal%20food%20contact%20material%20recycling/metal-food-contact-material-recycling",
+  },
+  {
+    id: "activePackaging",
+    name: "Overview and principles",
+    url: "https://fitness.agroparistech.fr/fitness2/lectures/lectures.html#Innovations/Active%20packaging/overview",
+  },
+  {
+    id: "cellulose",
+    name: "Advanced cellulosic materials for food packaging",
+    url: "https://fitness.agroparistech.fr/fitness2/lectures/lectures.html#Innovations/Advanced%20cellulosic%20materials/advanced-cellulosic-materials-for-food-packaging",
+  },
+  {
+    id: "bacterialPolyester",
+    name: "Bacterial Polyesters",
+    url: "https://fitness.agroparistech.fr/fitness2/lectures/lectures.html#Innovations/Bacterial%20polyesters/bacterial-polyesters",
+  },
+  {
+    id: "materialBioBased",
+    name: "Overview",
+    url: "https://fitness.agroparistech.fr/fitness2/lectures/lectures.html#Innovations/Biobased%20and%20biodegradable%20materials/from-polymers-to-bioplastics",
+  },
+  {
+    id: "modifiedAtmospherePackaging",
+    name: "Modified atmosphere packaging",
+    url: "https://fitness.agroparistech.fr/fitness2/lectures/lectures.html#Innovations/Modified%20atmosphere%20packaging/modified-atmosphere-packaging",
+  },
+  {
+    id: "nanomaterials",
+    name: "Definitions, nano-scale additives, examples of gains and applications.",
+    url: "https://fitness.agroparistech.fr/fitness2/lectures/lectures.html#Innovations/Nanotechnology%20strategies/definitions",
+  },
+  {
+    id: "smartPackaging",
+    name: "From principles to commercially available intelligent packaging systems",
+    url: "https://fitness.agroparistech.fr/fitness2/lectures/lectures.html#Innovations/Smart%20packaging/overview",
+  },
+  {
+    id: "regulatoryFramework",
+    name: "Regulatory framework in the EU and the US",
+    url: "https://fitness.agroparistech.fr/fitness2/lectures/lectures.html#Legislation/EU,%20US%20others%20regulations/regulatory-framework",
+  },
+  {
+    id: "gmpAndQualityAssurance",
+    name: "GMP, Safety, Quality management system, Responsibilities and Certification",
+    url: "https://fitness.agroparistech.fr/fitness2/lectures/lectures.html#Legislation/GMP%20and%20quality%20assurance%20standards/safety-and-quality-management-system",
+  },
+  {
+    id: "universeOfFoodContactChemicals",
+    name: "Universe of food contact chemicals",
+    url: "https://fitness.agroparistech.fr/fitness2/lectures/lectures.html#Legislation/Universe%20of%20food%20contact%20chemicals/universe-of-food-contact-chemicals",
+  },
+  {
+    id: "sensoryScience",
+    name: "Sensory science",
+    url: "https://fitness.agroparistech.fr/fitness2/lectures/lectures.html#Marketing%20and%20design/Sensory%20science/sensory-science",
+  },
+  {
+    id: "materialBioBased",
+    name: "Bio-based materials",
+    url: "https://fitness.agroparistech.fr/fitness2/lectures/lectures.html#Packaging%20materials/Packaging%20materials%20and%20shaping%20process/bio-based-materials",
+  },
+  {
+    id: "materialGlass",
+    name: "Glass",
+    url: "https://fitness.agroparistech.fr/fitness2/lectures/lectures.html#Packaging%20materials/Packaging%20materials%20and%20shaping%20process/glass-packaging",
+  },
+  {
+    id: "materialMetal",
+    name: "Metal",
+    url: "https://fitness.agroparistech.fr/fitness2/lectures/lectures.html#Packaging%20materials/Packaging%20materials%20and%20shaping%20process/metal-packaging",
+  },
+  {
+    id: "materialPaperAndBoard",
+    name: "Paper and paperboard",
+    url: "https://fitness.agroparistech.fr/fitness2/lectures/lectures.html#Packaging%20materials/Packaging%20materials%20and%20shaping%20process/paper-and-paperboard",
+  },
+  {
+    id: "materialPlastics",
+    name: "Plastic",
+    url: "https://fitness.agroparistech.fr/fitness2/lectures/lectures.html#Packaging%20materials/Packaging%20materials%20and%20shaping%20process/plastic-packaging",
+  },
+  {
+    id: "vacuumAndModifiedAtmospherePackaging",
+    name: "D-9 Vacuum and modified atmosphere packaging",
+    url: "https://fitness.agroparistech.fr/fitness2/lectures/lectures.html#Packaging%20processes/vacuum-and-modified-atmosphere-packaging",
+  },
+  {
+    id: "beverageFilling",
+    name: "D-5 Beverage filling",
+    url: "https://fitness.agroparistech.fr/fitness2/lectures/lectures.html#Packaging%20processes/Filling%20and%20dosing/beverage-filling",
+  },
+  {
+    id: "formingFillingAndSealingMachines",
+    name: "D-7 Forming, filling and sealing machines",
+    url: "https://fitness.agroparistech.fr/fitness2/lectures/lectures.html#Packaging%20processes/Filling%20and%20dosing/forming-filling-sealing",
+  },
+  {
+    id: "fillingAndDosing",
+    name: "D-4 Filling and dosing",
+    url: "https://fitness.agroparistech.fr/fitness2/lectures/lectures.html#Packaging%20processes/Filling%20and%20dosing/principles",
+  },
+  {
+    id: "formingOfPreProducts",
+    name: "D-2 Forming of pre-products",
+    url: "https://fitness.agroparistech.fr/fitness2/lectures/lectures.html#Packaging%20processes/Forming/plastic-forming",
+  },
+  {
+    id: "massTransferProperties",
+    name: "Mass transfer properties: methods and standards",
+    url: "https://fitness.agroparistech.fr/fitness2/lectures/lectures.html#Packaging%20properties/Thermal,%20mechanical%20and%20barrier%20properties/mass-transfer-properties",
+  },
+  {
+    id: "mechanicalProperties",
+    name: "Mechanical properties: methods and standards",
+    url: "https://fitness.agroparistech.fr/fitness2/lectures/lectures.html#Packaging%20properties/Thermal,%20mechanical%20and%20barrier%20properties/mechanical-properties",
+  },
+  {
+    id: "thermalProperties",
+    name: "Thermal properties: methods and standards",
+    url: "https://fitness.agroparistech.fr/fitness2/lectures/lectures.html#Packaging%20properties/Thermal,%20mechanical%20and%20barrier%20properties/thermal-properties",
+  },
+  {
+    id: "microbiologicalAssessmentOfFoodAndPackaging",
+    name: "Microbiological assessment of food and packaging",
+    url: "https://fitness.agroparistech.fr/fitness2/lectures/lectures.html#Shelf-life/Food%20packaging%20and%20shelf%20life/microbiological-assessment-of-food-and-packaging",
+  },
+  {
+    id: "shelfLifeStudy",
+    name: "Defining shelf life",
+    url: "https://fitness.agroparistech.fr/fitness2/lectures/lectures.html#Shelf-life/Food%20packaging%20and%20shelf%20life/study-shelf-life",
+  },
+];
