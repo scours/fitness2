e7766b8f55edd61a05cdfb884aed7a52043cc395
scours <32219601+scours@users.noreply.github.com>
2024-10-18 14:19:25 +0200

    add header banner with logos

--- quiz-creator-tool-online/src/js/header.js
+++ quiz-creator-tool-online/src/js/header.js
@@ -5,19 +5,46 @@
  * File Created: Tuesday, 21st February 2023
  * Author: Steward OUADI
  * -----
- * Last Modified: Tuesday, 17th September 2024
+ * Last Modified: Friday, 18th October 2024
  * Modified By: Steward OUADI
  */
 class Header extends HTMLElement {
   connectedCallback() {
     this.innerHTML = `
+    <div id="google_translate_element"></div> <!-- Google Translate element -->
     <div class="topnav" id="topNavId1">
         <a href="https://fitness.agroparistech.fr" >Home</a>
         <a href="javascript:void(0);" class="icon" onclick="topNavOnClick()">
             <i class="fa fa-bars"></i>
         </a>
     </div>
-    <div id="google_translate_element"></div> <!-- Google Translate element -->
+    <div class="logo-header">
+      <div class="logo-left">
+        <!-- FITNESS 2 Logo with link to home -->
+        <a href="/" title="Back to Home">
+          <img src="src/Fitness2_logo-no-background.png" alt="FITNESS 2 Logo" class="logo">
+        </a>
+      </div>
+
+      <div class="logo-right">
+        <!-- Coordinator Logo -->
+        <img src="src/coordinator-actia-logo-no-background.png" alt="Coordinator Logo" class="logo">
+
+        <!-- Twitter Logo with link -->
+        <a href="https://twitter.com/FitNESS_2_0" target="_blank" title="Twitter">
+          <img src="src/twitter-icon.png" alt="Twitter Logo" class="social-logo">
+        </a>
+
+        <!-- LinkedIn Logo with link -->
+        <a href="https://www.linkedin.com/company/fitness-packaging/" target="_blank" title="LinkedIn">
+          <img src="src/LinkedIn_icon.png" alt="LinkedIn Logo" class="social-logo">
+        </a>
+      </div>
+    </div>
+
+    <!-- Horizontal line -->
+    <div class="bottom-border"></div>
+
     `;
   }
 }
