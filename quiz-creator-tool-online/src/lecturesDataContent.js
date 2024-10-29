0517ce736f5d88f52ea830a30f88a05aa90639f9
scours <32219601+scours@users.noreply.github.com>
2024-10-29 17:12:07 +0100

    update lectures Urls

--- quiz-creator-tool-online/src/lecturesDataContent.js
+++ quiz-creator-tool-online/src/lecturesDataContent.js
@@ -3,9 +3,9 @@
  * Contract: EU contract 2022-FR01-KA220-HED-000023509
  * Project: FitNESS 2 ERASMUS+
  * File Created: Friday, 13th September 2024
- * Authors: Steward OUADI (AgroParisTech),  Olivier VITRAC (INRAE)
+ * Authors: Steward OUADI (AgroParisTech), Olivier VITRAC (INRAE)
  * -----
- * Last Modified: Tuesday, 15th October 2024
+ * Last Modified: Tuesday, 29th October 2024
  * Modified By: Steward OUADI
  */
 
@@ -31,6 +31,46 @@ const lecturesData = [
     url: "https://fitness.agroparistech.fr/fitness2/lectures/lectures.html#Ecodesign%20tools%20and%20technologies/metal-food-contact-material-recycling",
   },
   {
+    id: "biodegradation",
+    name: "Biodegradability and bio-based carbon content",
+    url: "https://fitness.agroparistech.fr/fitness2/lectures/lectures.html#Ecodesign%20tools%20and%20technologies/biodegradability-and-biobased-carbon-content",
+  },
+  {
+    id: "microplastics",
+    name: "Microplastics",
+    url: "https://fitness.agroparistech.fr/fitness2/lectures/lectures.html#Ecodesign%20tools%20and%20technologies/microplastics",
+  },
+  {
+    id: "plasticsRecycling",
+    name: "Plastics recycling",
+    url: "https://fitness.agroparistech.fr/fitness2/lectures/lectures.html#Ecodesign%20tools%20and%20technologies/plastics-recycling",
+  },
+  {
+    id: "reusableAndReturnableSystems",
+    name: "Reusable and returnable systems",
+    url: "https://fitness.agroparistech.fr/fitness2/lectures/lectures.html#Ecodesign%20tools%20and%20technologies/reusable-and-returnable-systems",
+  },
+  {
+    id: "ecodesignPackaging",
+    name: "Ecodesign applied to packaging",
+    url: "https://fitness.agroparistech.fr/fitness2/lectures/lectures.html#Ecodesign%20tools%20and%20technologies/Tools%20for%20life%20cycle%20impact%20assessment/ecodesign-applied-to-packaging",
+  },
+  {
+    id: "ecodesign",
+    name: "Ecodesign",
+    url: "https://fitness.agroparistech.fr/fitness2/lectures/lectures.html#Ecodesign%20tools%20and%20technologies/Tools%20for%20life%20cycle%20impact%20assessment/ecodesign",
+  },
+  {
+    id: "environmentalLegislation",
+    name: "Environmental legislation for packaging",
+    url: "https://fitness.agroparistech.fr/fitness2/lectures/lectures.html#Ecodesign%20tools%20and%20technologies/Tools%20for%20life%20cycle%20impact%20assessment/environmental-legislation-for-packaging",
+  },
+  {
+    id: "circularEconomy",
+    name: "Introduction to Circular Economy",
+    url: "https://fitness.agroparistech.fr/fitness2/lectures/lectures.html#Ecodesign%20tools%20and%20technologies/Tools%20for%20life%20cycle%20impact%20assessment/introduction-to-Circular-Economy",
+  },
+  {
     id: "activePackaging",
     name: "Overview and principles",
     url: "https://fitness.agroparistech.fr/fitness2/lectures/lectures.html#Innovative%20food%20packaging/active-packaging",
@@ -51,13 +91,8 @@ const lecturesData = [
     url: "https://fitness.agroparistech.fr/fitness2/lectures/lectures.html#Innovative%20food%20packaging/bioplastics",
   },
   {
-    id: "modifiedAtmospherePackaging",
-    name: "Modified atmosphere packaging",
-    url: "https://fitness.agroparistech.fr/fitness2/lectures/lectures.html#Packaging%20processes/vacuum-and-modified-atmosphere-packaging",
-  },
-  {
     id: "nanomaterials",
-    name: "Definitions, nano-scale additives, examples of gains and applications.",
+    name: "Definitions, nano-scale additives, examples of gains and applications",
     url: "https://fitness.agroparistech.fr/fitness2/lectures/lectures.html#Innovative%20food%20packaging/nanotechnology-strategies",
   },
   {
@@ -66,38 +101,68 @@ const lecturesData = [
     url: "https://fitness.agroparistech.fr/fitness2/lectures/lectures.html#Innovative%20food%20packaging/smart-packaging",
   },
   {
-    id: "regulatoryFramework",
-    name: "Regulatory framework in the EU and the US",
-    url: "https://fitness.agroparistech.fr/fitness2/lectures/lectures.html#Legislation%20and%20safety/eu-us-regulatory-framework",
-  },
-  {
-    id: "gmpAndQualityAssurance",
-    name: "GMP, Safety, Quality management system, Responsibilities and Certification",
-    url: "https://fitness.agroparistech.fr/fitness2/lectures/lectures.html#Legislation%20and%20safety/gmp-and-quality-assurance-standards",
-  },
-  {
     id: "universeOfFoodContactChemicals",
     name: "Universe of food contact chemicals",
     url: "https://fitness.agroparistech.fr/fitness2/lectures/lectures.html#Legislation%20and%20safety/universe-of-food-contact-chemicals",
   },
   {
-    id: "sensoryScience",
-    name: "Sensory science",
-    url: "https://fitness.agroparistech.fr/fitness2/lectures/lectures.html#Packaging%20and%20food%20shelf-life/sensory-science",
+    id: "foodPackagingInteractions",
+    name: "Food packaging interactions",
+    url: "https://fitness.agroparistech.fr/fitness2/lectures/lectures.html#Packaging%20and%20food%20shelf-life/food-packaging-interactions",
   },
   {
-    id: "materialBioBased",
+    id: "microbiologicalAssessmentOfFoodAndPackaging",
+    name: "Microbiological assessment of food and packaging",
+    url: "https://fitness.agroparistech.fr/fitness2/lectures/lectures.html#Packaging%20and%20food%20shelf-life/microbiological-assessment-of-food-and-packaging",
+  },
+  {
+    id: "modifiedAtmospherePackaging",
+    name: "Modified atmosphere packaging",
+    url: "https://fitness.agroparistech.fr/fitness2/lectures/lectures.html#Packaging%20and%20food%20shelf-life/modified-atmosphere-packaging",
+  },
+  {
+    id: "physicoChemicalFactorsFoodStability",
+    name: "Physico-chemical factors affecting food stability",
+    url: "https://fitness.agroparistech.fr/fitness2/lectures/lectures.html#Packaging%20and%20food%20shelf-life/physico-chemical-factors-affecting-food-stability",
+  },
+  {
+    id: "shelfLifeStudy",
+    name: "Defining shelf life",
+    url: "https://fitness.agroparistech.fr/fitness2/lectures/lectures.html#Packaging%20and%20food%20shelf-life/shelf-life-definition",
+  },
+  {
+    id: "advancedPrinciplesMassTransfer",
+    name: "Advanced principles of mass transfer in food packaging",
+    url: "https://fitness.agroparistech.fr/fitness2/lectures/lectures.html#Packaging%20material%20properties/advanced-principles-of-mass-transfer-in-food-packaging",
+  },
+  {
+    id: "massTransferProperties",
+    name: "Mass transfer properties: methods and standards",
+    url: "https://fitness.agroparistech.fr/fitness2/lectures/lectures.html#Packaging%20material%20properties/Thermal,%20mechanical%20and%20barrier%20properties/mass-transfer-properties",
+  },
+  {
+    id: "mechanicalProperties",
+    name: "Mechanical properties: methods and standards",
+    url: "https://fitness.agroparistech.fr/fitness2/lectures/lectures.html#Packaging%20material%20properties/Thermal,%20mechanical%20and%20barrier%20properties/mechanical-properties",
+  },
+  {
+    id: "thermalProperties",
+    name: "Thermal properties: methods and standards",
+    url: "https://fitness.agroparistech.fr/fitness2/lectures/lectures.html#Packaging%20material%20properties/Thermal,%20mechanical%20and%20barrier%20properties/thermal-properties",
+  },
+  {
+    id: "bioBasedMaterials",
     name: "Bio-based materials",
     url: "https://fitness.agroparistech.fr/fitness2/lectures/lectures.html#Packaging%20materials/bio-based-materials",
   },
   {
     id: "materialGlass",
-    name: "Glass",
+    name: "Glass packaging",
     url: "https://fitness.agroparistech.fr/fitness2/lectures/lectures.html#Packaging%20materials/glass-packaging",
   },
   {
     id: "materialMetal",
-    name: "Metal",
+    name: "Metal packaging",
     url: "https://fitness.agroparistech.fr/fitness2/lectures/lectures.html#Packaging%20materials/metal-packaging",
   },
   {
@@ -107,13 +172,13 @@ const lecturesData = [
   },
   {
     id: "materialPlastics",
-    name: "Plastic",
+    name: "Plastic packaging",
     url: "https://fitness.agroparistech.fr/fitness2/lectures/lectures.html#Packaging%20materials/plastic-packaging",
   },
   {
-    id: "vacuumAndModifiedAtmospherePackaging",
-    name: "Vacuum and modified atmosphere packaging",
-    url: "https://fitness.agroparistech.fr/fitness2/lectures/lectures.html#Packaging%20processes/vacuum-and-modified-atmosphere-packaging",
+    id: "asepticPackaging",
+    name: "Aseptic packaging",
+    url: "https://fitness.agroparistech.fr/fitness2/lectures/lectures.html#Packaging%20processes/aseptic-packaging",
   },
   {
     id: "beverageFilling",
@@ -121,14 +186,19 @@ const lecturesData = [
     url: "https://fitness.agroparistech.fr/fitness2/lectures/lectures.html#Packaging%20processes/beverage-filling",
   },
   {
+    id: "fillingAndDosing",
+    name: "Filling and dosing",
+    url: "https://fitness.agroparistech.fr/fitness2/lectures/lectures.html#Packaging%20processes/filling-and-dosing",
+  },
+  {
     id: "formingFillingAndSealingMachines",
     name: "Forming, filling and sealing machines",
     url: "https://fitness.agroparistech.fr/fitness2/lectures/lectures.html#Packaging%20processes/forming-filling-sealing",
   },
   {
-    id: "fillingAndDosing",
-    name: "Filling and dosing",
-    url: "https://fitness.agroparistech.fr/fitness2/lectures/lectures.html#Packaging%20processes/filling-and-dosing",
+    id: "mechanicalProcesses",
+    name: "Introduction to mechanical processes",
+    url: "https://fitness.agroparistech.fr/fitness2/lectures/lectures.html#Packaging%20processes/introduction-to-mechanical-processes",
   },
   {
     id: "formingOfPreProducts",
@@ -136,28 +206,18 @@ const lecturesData = [
     url: "https://fitness.agroparistech.fr/fitness2/lectures/lectures.html#Packaging%20processes/plastic-forming",
   },
   {
-    id: "massTransferProperties",
-    name: "Mass transfer properties: methods and standards",
-    url: "https://fitness.agroparistech.fr/fitness2/lectures/lectures.html#Packaging%20material%20properties/mass-transfer-properties",
+    id: "sealingAndWelding",
+    name: "Sealing and welding",
+    url: "https://fitness.agroparistech.fr/fitness2/lectures/lectures.html#Packaging%20processes/sealing",
   },
   {
-    id: "mechanicalProperties",
-    name: "Mechanical properties: methods and standards",
-    url: "https://fitness.agroparistech.fr/fitness2/lectures/lectures.html#Packaging%20material%20properties/mechanical-properties",
+    id: "surfaceTreatmentsAndCoatings",
+    name: "Surface treatments and coatings",
+    url: "https://fitness.agroparistech.fr/fitness2/lectures/lectures.html#Packaging%20processes/surface-treatments-and-coatings",
   },
   {
-    id: "thermalProperties",
-    name: "Thermal properties: methods and standards",
-    url: "https://fitness.agroparistech.fr/fitness2/lectures/lectures.html#Packaging%20material%20properties/thermal-properties",
-  },
-  {
-    id: "microbiologicalAssessmentOfFoodAndPackaging",
-    name: "Microbiological assessment of food and packaging",
-    url: "https://fitness.agroparistech.fr/fitness2/lectures/lectures.html#Packaging%20and%20food%20shelf-life/microbiological-assessment-of-food-and-packaging",
-  },
-  {
-    id: "shelfLifeStudy",
-    name: "Defining shelf life",
-    url: "https://fitness.agroparistech.fr/fitness2/lectures/lectures.html#Packaging%20and%20food%20shelf-life/study-shelf-life",
+    id: "vacuumAndModifiedAtmospherePackaging",
+    name: "Vacuum and modified atmosphere packaging",
+    url: "https://fitness.agroparistech.fr/fitness2/lectures/lectures.html#Packaging%20processes/vacuum-and-modified-atmosphere-packaging",
   },
 ];
